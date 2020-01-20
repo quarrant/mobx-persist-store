f4b35bc70d36d33575ad863e25fdc5184ef96c4e
dalekseev <dalekseev@kosmostyt.ru>
2020-01-20 14:30:23 +0300

    isSynchronized fixes

--- src/isSynchronized.ts
+++ src/isSynchronized.ts
@@ -1,5 +1,5 @@
 import StorageConfiguration from './StorageConfiguration';
 
 export default function isSynchronized<T extends Object>(target: T) {
-  return StorageConfiguration.getIsSynchronized(target);
+  return StorageConfiguration.getIsSynchronized(target.constructor.name);
 }
