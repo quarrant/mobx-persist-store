daca5fc89683ed22317799732273170f6f179f29
dalekseev <dalekseev@kosmostyt.ru>
2020-02-06 15:24:42 +0300

    fix useDisposers typings

--- src/useDisposers.ts
+++ src/useDisposers.ts
@@ -1,6 +1,6 @@
 import StorageConfiguration from './StorageConfiguration';
 
-export default function useDisposers<T extends Object>(target: T & { _storageName: string }) {
+export default function useDisposers<T extends Object>(target: T & { _storageName?: string }) {
   const disposers = StorageConfiguration.getDisposers(target._storageName || target.constructor.name);
   disposers.forEach((disposers) => disposers());
 }
