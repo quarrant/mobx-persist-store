427a701567a97dea9c9a60a449c235cb65260bb5
dalekseev <dalekseev@kosmostyt.ru>
2020-03-31 20:11:29 +0300

    fixes isPersistence every undefined

--- src/isPersistence.ts
+++ src/isPersistence.ts
@@ -1,5 +1,7 @@
 import { PersistenceStore } from './types';
 
-export default function isPersistence<T extends Object>(target: T): target is PersistenceStore<T> {
-  return !!target.hasOwnProperty('_isPersistence');
+export default function isPersistence<T extends { _isPersistence?: boolean }>(
+  target: T,
+): target is PersistenceStore<T> {
+  return !!target._isPersistence;
 }
