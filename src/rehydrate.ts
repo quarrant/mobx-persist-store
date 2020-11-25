03e2b1d416e1dbfc63d74eb7a4d7efe8be59c9d5
dm.alekseev <dm.alekseev@macbook-dm.alekseev>
2020-11-25 13:06:27 +0300

    added rehydrate function

---
+++ src/rehydrate.ts
@@ -0,0 +1,13 @@
+import { autorun } from 'mobx';
+import { isSynchronized } from './isSynchronized';
+
+export function rehydrate<T>(target: T) {
+  return new Promise((resolve) => {
+    const disposer = autorun(() => {
+      if (isSynchronized(target)) {
+        disposer();
+        resolve();
+      }
+    });
+  });
+}
