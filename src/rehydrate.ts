f8413ad10f9aefeea26d9a31a6f98206d75797a8
dm.alekseev <dm.alekseev@macbook-dm.alekseev>
2020-12-16 18:31:17 +0300

    fix rehydrate typing

--- src/rehydrate.ts
+++ src/rehydrate.ts
@@ -1,7 +1,7 @@
 import { autorun } from 'mobx';
 import { isSynchronized } from './isSynchronized';
 
-export function rehydrate<T>(target: T) {
+export function rehydrate<T>(target: T): Promise<void> {
   return new Promise((resolve) => {
     const disposer = autorun(() => {
       if (isSynchronized(target)) {
