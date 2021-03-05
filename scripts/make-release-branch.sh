8e47f2c759eaa2926a52f998b27ac6df62e5586d
d.alekseev <d.alekseev@MacBook-Pro-Admin.local>
2021-03-05 13:30:28 +0300

    add release-branch script

---
+++ scripts/make-release-branch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$BRANCH" != "master" ]]; then
+  echo 'Checkout to master branch!';
+  exit 1;
+fi
+
+VERSION=$(npm version patch --no-git-tag-version)
+
+git checkout -b release-$VERSION
+git add package*.json
+
+git commit -m "release-$VERSION"
+git push
