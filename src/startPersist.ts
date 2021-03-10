5ca292c119b604cda44a77d84014731fbcbb8d88
Dmitry Alekseev <44493574+quarrant@users.noreply.github.com>
2021-03-10 10:11:06 +0300

    Update startPersist.ts

--- src/startPersist.ts
+++ src/startPersist.ts
@@ -4,7 +4,7 @@ import { isPersistence } from './isPersistence';
 export function startPersist<T extends Object>(target: T) {
   if (isPersistence(target)) {
     const disposers = StorageConfiguration.getDisposers(target);
-    if (disposers.length) {
+    if (!disposers.length) {
       const startPersistFunction = StorageConfiguration.getStartPersist(target);
       startPersistFunction?.();
     }
