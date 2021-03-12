118c885fe5d43f5986cee7534a33ac35c9bfae87
Dmitry Alekseev <quarrant@gmail.com>
2021-03-13 00:42:04 +0300

    try new ci

--- scripts/make-release-branch.sh
+++ scripts/make-release-branch.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if [[ "$BRANCH" != "master" ]]; then
+branch="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$branch" != "master" ]]; then
   echo 'Checkout to master branch!';
   exit 1;
 fi
 
-VERSION=$(npm version patch --no-git-tag-version)
+version=$(npm version patch --no-git-tag-version)
 
-git checkout -b release-$VERSION
 git add package*.json
+git commit -m "release-$version"
 
-git commit -m "release-$VERSION"
-git push -u origin release-$VERSION
-
-git checkout master
+git tag $version
+git push origin --tags
