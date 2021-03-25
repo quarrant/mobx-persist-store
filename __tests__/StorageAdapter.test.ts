0e710e87353579b68c902f3dc6c5c7d52226594e
codeBelt <robertsavian@gmail.com>
2021-03-25 09:28:36 -0500

    Add TypeScript to Jest

--- __tests__/StorageAdapter.test.ts
+++ __tests__/StorageAdapter.test.ts
@@ -1,17 +1,18 @@
 const { StorageAdapter } = require('../src/StorageAdapter');
 
-const testStorage = {
+const testStorage: Record<string, unknown> = {
   testRead: JSON.stringify({ test: 'testContent' }),
+  mockStore: undefined,
 };
 
-function testWriteInJson(name, content) {
+function testWriteInJson(name: string, content: Record<string, unknown>): Promise<void> {
   return new Promise((resolve) => {
     Object.assign(testStorage, { [name]: content });
     resolve();
   });
 }
 
-function testReadFromJson(name) {
+function testReadFromJson(name: string) {
   return new Promise((resolve) => {
     if (testStorage[name]) {
       resolve(testStorage[name]);
@@ -19,30 +20,29 @@ function testReadFromJson(name) {
   });
 }
 
-const storage = new StorageAdapter({ write: testWriteInJson, read: testReadFromJson })
+const storage = new StorageAdapter({ write: testWriteInJson, read: testReadFromJson });
 
-const mockStore = {
-  4: "test",
+const mockStore: Record<string, unknown> = {
+  4: 'test',
   5: 1,
   6: 1.15,
   7: { 0: 'a' },
   8: [1, 1],
-  9: 1e15
-}
+  9: 1e15,
+};
 
 describe('mock store', () => {
-
   beforeAll(() => {
-    storage.writeInStorage('mockStore', mockStore)
-  })
-  
+    storage.writeInStorage('mockStore', mockStore);
+  });
+
   it(`writeInStore`, () => {
     expect(testStorage['mockStore']).toEqual(JSON.stringify(mockStore));
-  })
+  });
 
   it(`readFromStore`, () => {
-    storage.readFromStorage('mockStore').then((content) => {
+    storage.readFromStorage('mockStore').then((content: Record<string, unknown>) => {
       expect(content).toEqual(mockStore);
     });
-  })
-})
+  });
+});
