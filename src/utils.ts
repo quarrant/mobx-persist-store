f1693d326a58d436d8a45121dc99f141c4d31441
codeBelt <robertsavian@gmail.com>
2021-03-27 20:42:18 -0500

    remove unused code

--- src/utils.ts
+++ src/utils.ts
@@ -8,10 +8,3 @@ export const hasTimestampExpired = (milliseconds: number): boolean => {
 
   return dateTimeExpiration <= dateTimeNow;
 };
-
-// A function that performs no operations.
-// export const noop = Function.prototype as <T>(...params: T[]) => T;
-//
-// export const delay = (durationInMilliseconds: number = 250): Promise<void> => {
-//   return new Promise((resolve) => setTimeout(resolve, durationInMilliseconds));
-// };
