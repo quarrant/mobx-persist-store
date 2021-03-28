148ca913e2ed1d1a5bdaf9e3e47486a2aacdc098
codeBelt <robertsavian@gmail.com>
2021-03-27 20:29:28 -0500

    Update Docs

--- src/types.ts
+++ src/types.ts
@@ -9,11 +9,44 @@ export type PersistenceOptions = {
 };
 
 export type StorageAdapterOptions = {
-  // jsonify bool Enables serialization as JSON
-  jsonify?: boolean; // default true
-  removeOnExpiration?: boolean; // default true
-  expiration?: number; // milliseconds
+  /**
+   * @property {Boolean} [jsonify] When true the data will be JSON.stringify before being passed to setItem. The default value is true.
+   * @default true
+   */
+  jsonify?: boolean;
+  /**
+   * @property {Number} [expiration] A value in milliseconds to determine when the data in storage should not be retrieved by getItem.
+   *
+   * Recommend the library https://github.com/henrikjoreteg/milliseconds to set the value
+   */
+  expiration?: number;
+  /**
+   * @property {Number} [removeOnExpiration] If {@link StorageAdapterOptions#expiration} has a value and has expired the data in storage will be remove automatically when getItem is called. The default value is true.
+   * @default true
+   */
+  removeOnExpiration?: boolean;
+  /**
+   * The function that will retrieved the storage data by a specific identifier.
+   *
+   * @function
+   * @param {String} key
+   * @return {Promise<String | Object>}
+   */
   getItem: <T>(key: string) => Promise<string | T>;
+  /**
+   * The function that will save data to the storage by a specific identifier.
+   *
+   * @function
+   * @param {String} key
+   * @return {Promise<void>}
+   */
   setItem: (key: string, item: any) => Promise<void>;
+  /**
+   * The function that will remove data from storage by a specific identifier.
+   *
+   * @function
+   * @param {String} key
+   * @return {Promise<void>}
+   */
   removeItem: (key: string) => Promise<void>;
 };
