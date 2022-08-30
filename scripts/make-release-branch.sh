aa9da6b1a2cfff36bd0f98f0ea941512888fe8b8
quarrant <quarrant@gmail.com>
2022-08-30 22:26:29 +0300

    make-release-branch script

--- scripts/make-release-branch.sh
+++ scripts/make-release-branch.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+type="patch"
+if [[ "$1" != "" ]]; then
+  type=$1
+fi
+
 branch="$(git rev-parse --abbrev-ref HEAD)"
 if [[ "$branch" != "master" ]]; then
   echo 'Checkout to master branch!';
   exit 1;
 fi
 
-version=$(npm version patch --no-git-tag-version)
+version=$(npm version $type --no-git-tag-version)
 
 git add package*.json
 git commit -m "release-$version"
