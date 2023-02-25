a71bd02614056874ddcfa8f0d57ba924b88622a9
Jon Woyame <jon@etchgis.com>
2023-02-24 21:09:54 -0500

    isFunction handles additional situations

--- src/utils.ts
+++ src/utils.ts
@@ -29,7 +29,7 @@ export const isObjectWithProperties = (data: any): boolean => {
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const isFunction = (functionToCheck: any): boolean => {
-  return functionToCheck && functionToCheck instanceof Function;
+  return functionToCheck && typeof functionToCheck === 'function';
 };
 
 export const isStorageControllerLike = (value: StorageController | Storage | undefined): value is StorageController => {
